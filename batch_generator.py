4e30f3df4136b6d34ca7fd77316243d5cccb5029
eleanna vali <eleannaval1996@gmail.com>
2019-09-04 14:32:03 +0000

    Upload New File

---
+++ batch_generator.py
@@ -0,0 +1,42 @@
+import numpy as np
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(28,28), n_channels=1,
+                 n_classes=10, shuffle=True):
+
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+
+    def __getitem__(self, index):
+        #Generate one batch of data
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size] # Generate indexes of the batch
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        X, y = self.__data_generation(list_IDs_temp)
+        return X, y
+
+    def on_epoch_end(self):
+        #Updates indexes after each epoch
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        #Generates data containing batch_size samples: X(n_samples, *dim, n_channels)
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        for i, ID in enumerate(list_IDs_temp):
+            X[i,] = np.load('path/' + ID + '.ending')
+            y[i] = self.labels[ID]
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
