e2f6191aa30fab8e4dc19eb25908bd02c05bab72
1996 to the moon <eleannaval1996@gmail.com>
2019-09-12 17:31:14 +0300

    change 'setup.py'

--- setup.py
+++ setup.py
@@ -1,39 +1,29 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-
-    long_description = fh.read()
-
-setuptools.setup(
-
-     name='adapy',  
-
-     version='0.1',
-
-     scripts=['adaptation.py','auxilliary.py','batch_generator.py'] ,
-
-     author="George Pikramenos, Eleanna Vali",
-
-     author_email="gpik@di.uoa.gr",
-
-     description="Package for Adversarial Domain Adaptation",
-
-     long_description=long_description,
-
-   long_description_content_type="text/markdown",
-
-     url="git@gitlab.com:cillab/adapy.git",
-
-     packages=setuptools.find_packages(),
-
-     classifiers=[
-
-         "Programming Language :: Python :: 3",
-
-         "License :: OSI Approved :: MIT License",
-
-         "Operating System :: OS Independent",
-
-     ],
-
- )
+from distutils.core import setup
+setup(
+  name = 'adapy',
+  packages = ['adapy'],
+  version = '0.1',
+  license='MIT',
+  description = 'Library for domain adaptation',
+  author = 'George Pikramenos, Eleanna Vali',
+  author_email = 'gpik@di.uoa.gr',
+  url = 'https://gitlab.com/cillab/adapy',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # not ready!!!!!
+  keywords = ['adaptation'],   # Keywords that define our package best
+  install_requires=[
+          'keras',
+          'tqdm',
+          'numpy',
+          'imageio'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',    
+    'License :: OSI Approved :: MIT License',    
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
